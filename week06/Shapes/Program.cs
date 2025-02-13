66ea39fd7589122e163a92cc21dfe1b13bd33a69
alexcazanobe <cazanobealex@outlook.com.ar>
2025-02-13 16:09:25 -0300

    I finished the learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 4.0);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 3.0, 6.0);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle ("Violet", 8.4);
+        shapes.Add(circle);
+
+        
+
+        foreach (var shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+            
+        }
+
     }
 }
